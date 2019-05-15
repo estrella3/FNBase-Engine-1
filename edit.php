c4621fd007b2ce6017dd73e8949dfb9f1d7de723
piedots <39951788+estrella3@users.noreply.github.com>
2019-05-15 15:08:59 +0900

    0.5.15
    SCEditor > Summernote

--- edit.php
+++ edit.php
@@ -20,20 +20,27 @@ echo '<div class="container"><form action="keepedit.php" method="post">
 <hr>
 <p><input type="text" style="outline: 0; width: 100%; border: none; background-color: transparent" value="'.$t.'" name="title" placeholder="제목" required></p>
 <hr>
-<textarea name="description" id="editor" style="width: 100%; border: none; background-color: white; min-height: 350px">'.$n.'</textarea>
-<script src="assets/minified/formats/bbcode.min.js"></script>
-<script>
-// Replace the textarea #example with SCEditor
-var textarea = document.getElementById("editor");
-sceditor.create(textarea, {
-format: "bbcode",
-toolbar: "bold,italic,strike,superscript|left,center,right|color,size,font|bulletlist,orderedlist,table|link,image,youtube|source,maximize",
-fonts: "",
-style: "assets/minified/themes/content/default.min.css"
-});
-</script>
-
-<p align="right"><br><input class="btn btn-success" style="width: 100%" type="submit" value="작성 완료"></p></div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-lite.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-lite.js"></script>
+<textarea id="summernote" name="description">'.$n.'</textarea>';
+echo "<script>
+              $('#summernote').summernote({
+        placeholder: '내용 작성',
+        tabsize: 2,
+        height: 300,
+        lang: 'ko-KR',
+        focus: true,
+		shortcuts: false,
+			toolbar: [
+			['style', ['bold', 'italic', 'underline', 'strikethrough', 'color', 'fontsize', 'clear']],
+			['para', ['ul', 'ol', 'paragraph']],
+			['insert', ['video', 'link', 'table', 'hr']],
+			['misc', ['codeview', 'undo', 'redo']]
+		]
+              });
+    </script>";
+echo '<p align="right"><br><input class="btn btn-success" style="width: 100%" type="submit" value="작성 완료"></p></div>
 <input type="hidden" name="id" value="'.$id.'">
 <input type="hidden" name="b" value="'.$b.'">
 </form></div>';
