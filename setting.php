1fe84971fb5f36c08f545fb2e71426f466fb0517
Estrella3 <admin@fnbase.xyz>
2019-09-26 19:53:42 +0900

    1.0.0

--- setting.php
+++ setting.php
@@ -1,7 +1,7 @@
 <?php
 # 이 파일은 FNBase.xyz에서 배포하는 게시글 관리 엔진의 설정 파일입니다.
 # 자세한 설정에 관해서는 https://dev.fnbase.xyz/fnbe.html 을 참고하십시오.
-$setVersion = "0.9.25"; #세팅 파일이 작성될 때의 버전입니다.
+$setVersion = "1.0.0"; #세팅 파일이 작성될 때의 버전입니다.
 
 #데이터베이스 연결 설정입니다.
 $fnSiteDB = "";
